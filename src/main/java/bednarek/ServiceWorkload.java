90a3eb896f48a5c501d7fe63c5bc8b8ff2387a93
nadia <nadia@com.ua>
2025-03-04 11:40:39 +0100

    testng unit tests

---
+++ src/main/java/bednarek/ServiceWorkload.java
@@ -0,0 +1,18 @@
+package bednarek;
+
+public enum ServiceWorkload {
+    VERY_HIGH(1.6),
+    HIGH(1.4),
+    INCREASED(1.2),
+    NORMAL(1);
+
+    private final double deliveryRate;
+
+    ServiceWorkload(double deliveryRate) {
+        this.deliveryRate = deliveryRate;
+    }
+
+    public double getDeliveryRate() {
+        return deliveryRate;
+    }
+}
